6d8dd5e53de2dc507583fb9aee9d9f9c5ef2b33a
piresz0 <166669801+piresz0@users.noreply.github.com>
2025-05-05 10:03:28 -0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 fun main() {
     print("Digite um número acima de 1: ")
-    var num = readln().toIntOrNull() ?: return
-    var fib = 0
+    val n = readln().toIntOrNull() ?: return
 
-    repeat(num) {
-        print("$fib ")
-        fib = (fib + (fib - 1)).takeIf { it > 0 } ?: 1
+    var fib: Pair<Int, Int> = 0 to 1
+
+    repeat(n) {
+        print("${fib.first} ")
+        fib = fib.second to (fib.first + fib.second)
     }
 }
